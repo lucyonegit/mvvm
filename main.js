e9ec0f0d565318750732067d7baaedb40e706382
lucy <18629049410@163.com>
2018-07-16 15:44:18 +0800

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //mvvm构造类
-let mvvm = function(options) {
+let mvvm = function (options) {
   this.options = options;
   this.el = options.el;
   this.data = options.data;
@@ -8,7 +8,7 @@ let mvvm = function(options) {
   this.compile(this.el); //编译文档
 };
 //实现observer
-mvvm.prototype.observe = function(obj) {
+mvvm.prototype.observe = function (obj) {
   let dep = new Dep();
   for (let key in obj) {
     if (obj.hasOwnProperty(key)) {
@@ -19,14 +19,14 @@ mvvm.prototype.observe = function(obj) {
       Object.defineProperty(this, key, {
         enumerable: true,
         configurable: true,
-        get: function() {
+        get: function () {
           console.log(`获取${value}`);
           if (Dep.target) {
             dep.addSub(Dep.target);
           } // 将watcher添加到订阅事件中 [watcher]
           return this.data[key]; //数据代理
         },
-        set: function(newVal) {
+        set: function (newVal) {
           console.log(`更新${newVal}`);
           if (value !== newVal) {
             this.data[key] = newVal;
@@ -41,7 +41,7 @@ mvvm.prototype.observe = function(obj) {
 };
 
 //实现compile
-mvvm.prototype.compile = function(el) {
+mvvm.prototype.compile = function (el) {
   let dom = document.querySelector(el);
   let fragment = document.createDocumentFragment();
   while ((child = dom.firstChild)) {
@@ -54,15 +54,11 @@ mvvm.prototype.compile = function(el) {
       let reg = /\{\{(.+?)\}\}/g; // 正则匹配是文本节点又有大括号的情况{{}}
       if (node.nodeType === 3 && reg.test(txt)) {
         console.log(RegExp.$1); //匹配data数据标记
-        let arr = RegExp.$1.split(".");
-        let value = that;
-        arr.forEach(key => {
-          value = value[key]; //调用属性get方法
-        });
-        node.textContent = txt.replace(reg, value).trim();
         new Watcher(that, RegExp.$1, newVal => {
-          node.textContent = txt.replace(reg, newVal).trim(); // 用trim方法去除一下首尾空格
+          node.textContent = txt.replace(reg, newVal).trim();
         });
+        let v = that[RegExp.$1]; //调用属性get方法
+        node.textContent = txt.replace(reg, v).trim();
       }
       // 如果还有子节点，继续递归replace
       if (node.childNodes && node.childNodes.length) {
@@ -94,22 +90,22 @@ class Watcher {
     Dep.target = this;
     //this.get();
   }
-  get() {
-    let arr = this.exp.split(".");
-    let val = this.mvvm;
-    arr.forEach(key => {
-      val = val[key]; //调用属性get方法
-    });
-    Dep.target = null;
-  }
+  // get() {
+  //   let arr = this.exp.split(".");
+  //   let val = this.mvvm;
+  //   arr.forEach(key => {
+  //     val = val[key]; //调用属性get方法
+  //   });
+  //   Dep.target = null;
+  // }
 }
-Watcher.prototype.update = function() {
+Watcher.prototype.update = function () {
   let arr = this.exp.split(".");
   let value = this.mvvm;
   arr.forEach(key => {
     value = value[key]; // 通过get获取到新的值
   });
-  this.fn(value); // 每次拿到的新值去替换{{}}的内容即可
+  this.fn(value); // 每次拿到的新值去替换{{}}的内容
 };
 
 //实例化对象
@@ -121,7 +117,7 @@ let app = new mvvm({
     point: { a: "11", b: "22" }
   },
   methods: {
-    increment: function() {
+    increment: function () {
       this.number++;
     }
   }
